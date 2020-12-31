d845355512fde6861378c954bb202c8d72c607f3
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2020-12-31 03:41:14 -0500

    log in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './view/components/nav-bar/nav-bar.component';
 import { SignUpPageComponent } from './view/pages/sign-up-page/sign-up-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    SignUpPageComponent
-  ],
+  declarations: [AppComponent, NavBarComponent, SignUpPageComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/view/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/view/pages/sign-up-page/sign-up-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
-  styleUrls: ['./sign-up-page.component.scss']
+  styleUrls: ['./sign-up-page.component.scss'],
 })
 export class SignUpPageComponent implements OnInit {
+  items: any;
+  checkoutForm;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    // private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
   }
 
+  ngOnInit() {
+    // this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData: any) {
+    // Process checkout data here
+    // this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
+  }
 }
